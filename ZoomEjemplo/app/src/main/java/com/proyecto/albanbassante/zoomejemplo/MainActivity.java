adf1cf05bcf6201bb31d081d293ab39ecbae9b3e
Gonzalo Albán R <gonzaloalban@server.pretendco.es>
2015-11-10 07:59:10 -0500

    cambio main

--- ZoomEjemplo/app/src/main/java/com/proyecto/albanbassante/zoomejemplo/MainActivity.java
+++ ZoomEjemplo/app/src/main/java/com/proyecto/albanbassante/zoomejemplo/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.util.FloatMath;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.MenuItem;   
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
